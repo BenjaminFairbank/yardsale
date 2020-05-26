2b92305befa8aae234818b24d90dbb5fbafd441f
BenjaminFairbank <BenFairbank26@gmail.com>
2020-05-25 20:52:37 -0400

    Making use of database cleaner, test for items content

--- spec/controllers/api/v1/items_controller_spec.rb
+++ spec/controllers/api/v1/items_controller_spec.rb
@@ -3,20 +3,37 @@ require "rails_helper"
 RSpec.describe Api::V1::ItemsController, type: :controller do
 
   describe "GET#index" do
-    user1 = FactoryBot.create(:user)
+    before(:example) do
+      @user = FactoryBot.create(:user)
+      @item1 = FactoryBot.create(:item)
+      @item2 = FactoryBot.create(:item)
+      @item3 = FactoryBot.create(:item)
+    end
 
-
-    it "should not list posts if the user is not logged in" do
+    it "should not list items if the user is not logged in" do
       get :index
       expect(response.status).to eq(302)
       expect(response).to redirect_to(new_user_session_path)
     end
 
     it "should allow user to view the list of items" do
-      sign_in(user1)
+      sign_in(@user)
       get :index
+
       expect(response.status).to eq 200
       expect(response.content_type).to eq ("application/json")
+    end
+
+    it "returns all items in the database" do
+      sign_in(@user)
+      get :index
+
+      returned_json = JSON.parse(response.body)
+
+      expect(returned_json["items"].length).to eq 3
+      expect(returned_json["items"][0]["name"]).to eq @item1.name
+      expect(returned_json["items"][1]["name"]).to eq @item2.name
+      expect(returned_json["items"][2]["name"]).to eq @item3.name
     end
 
   end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,11 +3,20 @@ require 'factory_bot'
 FactoryBot.define do
   factory :user do
     sequence(:email) {|n| "user#{n}@example.com" }
-    user_name { |n| "user#{n}" }
+    sequence(:user_name) { |n| "user#{n}" }
     zip_code { "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" }
     password { 'password' }
     password_confirmation { 'password' }
     profile_photo { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/images/photo.png'), 'image/png') }
+  end
+
+  factory :item do
+    association :user, factory: :user
+    sequence(:name) { |n| "item#{n}" }
+    sequence(:description) { |n| "description#{n}" }
+    asking_price { "#{rand(999999999)}"}
+    zip_code { "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" + "#{rand(9)}" }
+    image { Rack::Test::UploadedFile.new(Rails.root.join('spec/support/images/photo.png'), 'image/png') }
   end
 
 end
