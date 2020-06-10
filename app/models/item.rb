8d82e45956eede761ac94c44b66b30edbf62b684
BenjaminFairbank <BenFairbank26@gmail.com>
2020-06-10 18:26:13 -0400

    Dependent models destroyed if parent is destroyed

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :name,          presence: true, length: { maximum: 20 }
   validates :description,   presence: true, length: { maximum: 200 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,8 +9,8 @@ class User < ApplicationRecord
   validates :zip_code, presence: true
   validates :profile_photo, presence: true
 
-  has_many :items
-  has_many :comments
+  has_many :items, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   mount_uploader :profile_photo, ProfilePhotoUploader
   validates_presence_of :profile_photo
