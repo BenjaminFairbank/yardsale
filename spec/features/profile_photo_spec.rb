be53f942d968583483b5948c3c096d173cd1859c
BenjaminFairbank <BenFairbank26@gmail.com>
2020-06-10 14:02:31 -0400

    Codeship fixes

--- spec/features/profile_photo_spec.rb
+++ spec/features/profile_photo_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 feature "profile photo" do
   scenario "user uploads a profile photo" do
     visit root_path
-    click_link "Create Account"
+    click_link "Create a new account"
 
     fill_in "Email", with: "thesingingsandwich@cramular.com"
     fill_in "User name", with: "BingoBango"
@@ -11,7 +11,7 @@ feature "profile photo" do
     fill_in "Password confirmation", with: "boomstick!3vilisd3ad"
     fill_in "Zip code", with: "02345"
     attach_file :user_profile_photo, "#{Rails.root}/spec/support/images/photo.png"
-    
+
     click_button "Create your new account"
 
     expect(page).to have_content("Welcome! You have signed up successfully.")
