f34190e94a848e1d5b72423a0a6bac329ec19925
BenjaminFairbank <BenFairbank26@gmail.com>
2020-07-04 23:38:43 -0400

    User content review page added for admins with delete capabilities

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -1,24 +1,15 @@
-class UsersController < ApplicationController
+class Admin::CommentsController < ApplicationController
 
   before_action :authenticate_user!, :authorize_user
 
-  def index
-    get_users
-    render :index
-  end
-
   def destroy
-    user = User.find(params[:id])
-    user.delete
-    get_users
-    render :index
+    comment = Comment.find(params[:id])
+    @user = comment.user
+    comment.delete
+    render "/admin/users/show"
   end
 
   private
-
-  def get_users
-    @Users = User.all
-  end
 
   def authorize_user
     if !user_signed_in? || !current_user.admin?
