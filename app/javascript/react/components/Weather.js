b46b6e744c8f8cdc647d8afdf102946879b66f76
BenjaminFairbank <BenFairbank26@gmail.com>
2020-07-06 02:05:04 -0400

    Will now generate error message if city not found rather than breaking

--- app/javascript/react/components/Weather.js
+++ app/javascript/react/components/Weather.js
@@ -2,28 +2,38 @@ import React from 'react'
 
 const Weather = props => {
 
-  let wD = ''
-  if ( props.weatherData.windDirection >= 337.5 || props.weatherData.windDirection < 22.5 ) {
-    wD = "wind " + 'N'
-  } else if ( props.weatherData.windDirection >= 22.5 && props.weatherData.windDirection < 67.5 ) {
-    wD = "wind " + 'NE'
-  } else if ( props.weatherData.windDirection >= 67.5 && props.weatherData.windDirection < 112.5 ) {
-    wD = "wind " + 'E'
-  } else if ( props.weatherData.windDirection >= 112.5 && props.weatherData.windDirection < 157.5 ) {
-    wD = "wind " + 'SE'
-  } else if ( props.weatherData.windDirection >= 157.5 && props.weatherData.windDirection < 202.5 ) {
-    wD = "wind " + 'S'
-  } else if ( props.weatherData.windDirection >= 202.5 && props.weatherData.windDirection < 247.5 ) {
-    wD = "wind " + 'SW'
-  } else if ( props.weatherData.windDirection >= 247.5 && props.weatherData.windDirection < 292.5 ) {
-    wD = "wind " + 'W'
-  } else if ( props.weatherData.windDirection >= 292.5 && props.weatherData.windDirection < 337.5 ) {
-    wD = "wind " + 'NW'
+  let weatherOutput = <p>Loading local weather data...</p>
+  
+  if ( props.weatherData.responseCode ) {
+    if ( props.weatherData.responseCode === 200 ) {
+      let wD = ''
+      if ( props.weatherData.windDirection >= 337.5 || props.weatherData.windDirection < 22.5 ) {
+        wD = "wind " + 'N'
+      } else if ( props.weatherData.windDirection >= 22.5 && props.weatherData.windDirection < 67.5 ) {
+        wD = "wind " + 'NE'
+      } else if ( props.weatherData.windDirection >= 67.5 && props.weatherData.windDirection < 112.5 ) {
+        wD = "wind " + 'E'
+      } else if ( props.weatherData.windDirection >= 112.5 && props.weatherData.windDirection < 157.5 ) {
+        wD = "wind " + 'SE'
+      } else if ( props.weatherData.windDirection >= 157.5 && props.weatherData.windDirection < 202.5 ) {
+        wD = "wind " + 'S'
+      } else if ( props.weatherData.windDirection >= 202.5 && props.weatherData.windDirection < 247.5 ) {
+        wD = "wind " + 'SW'
+      } else if ( props.weatherData.windDirection >= 247.5 && props.weatherData.windDirection < 292.5 ) {
+        wD = "wind " + 'W'
+      } else if ( props.weatherData.windDirection >= 292.5 && props.weatherData.windDirection < 337.5 ) {
+        wD = "wind " + 'NW'
+      }
+
+      weatherOutput = <p>Local Weather{props.weatherData.description} {props.weatherData.currentTemperature} {props.weatherData.humidity} {wD} {props.weatherData.wind} {props.weatherData.cloudCover}</p>
+    } else {
+      weatherOutput = <p>The OpenWeatherMap API failed to provide weather data for your zip code: {props.weatherData.errorMessage}</p>
+    }
   }
 
   return (
     <div id="weather">
-      <p>Local Weather{props.weatherData.description} {props.weatherData.currentTemperature} {props.weatherData.humidity} {wD} {props.weatherData.wind} {props.weatherData.cloudCover}</p>
+      {weatherOutput}
     </div>
   )
 }
