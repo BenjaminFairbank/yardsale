d1afcf2186c76c24e475d1e12973ff7a20929d90
BenjaminFairbank <BenFairbank26@gmail.com>
2020-09-07 21:34:27 +0800

    Tests for admin users controller (show) added

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -42,4 +42,43 @@ RSpec.describe Admin::UsersController, type: :controller do
       expect(assigns(:users)).to eq(User.all)
     end
   end
+
+  describe "GET#show" do
+
+    before(:example) do
+      @user = FactoryBot.create(:user)
+      @user.update_attribute("role", "admin")
+      @user1 = FactoryBot.create(:user)
+    end
+
+    it "should not render the template if the current user is not logged in" do
+      get :show, params: {id: @user1.id}
+      expect(response.status).to eq(302)
+      expect(flash[:alert]).to eq("")
+      expect(response).to redirect_to(new_user_session_path)
+      expect(response).not_to render_template(:show)
+    end
+
+    it "should not render the template if the current user is not an admin" do
+      sign_in(@user1)
+      get :show, params: {id: @user1.id}
+      expect(response.status).to eq(302)
+      expect(flash[:notice]).to eq("You do not have access to this page.")
+      expect(response).not_to render_template(:show)
+    end
+
+    it "should render the template if the current user is an admin" do
+      sign_in(@user)
+      get :show, params: {id: @user1.id}
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq ("text/html")
+      expect(response).to render_template(:show)
+    end
+
+    it "should provide the target user's information" do
+      sign_in(@user)
+      get :show, params: {id: @user1.id}
+      expect(assigns(:user)).to eq(User.find(@user1.id))
+    end
+  end
 end
