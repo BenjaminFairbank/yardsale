e76a5ecf3e59370e33577528054ed74ffab7e6ff
BenjaminFairbank <BenFairbank26@gmail.com>
2020-09-07 22:02:16 +0800

    Tests for admin users controller (update) added

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -81,4 +81,57 @@ RSpec.describe Admin::UsersController, type: :controller do
       expect(assigns(:user)).to eq(User.find(@user1.id))
     end
   end
+
+  describe "PATCH#update" do
+
+    before(:example) do
+      @user = FactoryBot.create(:user)
+      @user.update_attribute("role", "admin")
+      @user1 = FactoryBot.create(:user)
+      @user1.update_attribute("role", "admin")
+      @user2 = FactoryBot.create(:user)
+    end
+
+    it "should not render the template if the current user is not logged in" do
+      patch :update, params: {id: @user2.id}
+      expect(response.status).to eq(302)
+      expect(flash[:alert]).to eq("")
+      expect(response).to redirect_to(new_user_session_path)
+      expect(response).not_to render_template(:index)
+    end
+
+    it "should not render the template if the current user is not an admin" do
+      sign_in(@user2)
+      patch :update, params: {id: @user2.id}
+      expect(response.status).to eq(302)
+      expect(flash[:notice]).to eq("You do not have access to this page.")
+      expect(response).not_to render_template(:index)
+    end
+
+    it "should render the template if the current user is an admin" do
+      sign_in(@user)
+      patch :update, params: {id: @user2.id}
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq ("text/html")
+      expect(response).to render_template(:index)
+    end
+
+    it "should make the user an admin if they are not already" do
+      sign_in(@user)
+      patch :update, params: {id: @user2.id}
+      expect(User.find(@user2.id).role).to eq("admin")
+    end
+
+    it "should make the user a member if they are already an admin" do
+      sign_in(@user)
+      patch :update, params: {id: @user1.id}
+      expect(User.find(@user1.id).role).to eq("member")
+    end
+
+    it "should provide an updated list of all the users" do
+      sign_in(@user)
+      patch :update, params: {id: @user2.id}
+      expect(assigns(:users)).to eq(User.all)
+    end
+  end
 end
