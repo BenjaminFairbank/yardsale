b165cd7023ecbd55d36d951861c8084d5b9ca024
BenjaminFairbank <BenFairbank26@gmail.com>
2020-09-07 22:28:45 +0800

    Tests for admin users controller (destroy) added

--- spec/controllers/admin/users_controller_spec.rb
+++ spec/controllers/admin/users_controller_spec.rb
@@ -134,4 +134,54 @@ RSpec.describe Admin::UsersController, type: :controller do
       expect(assigns(:users)).to eq(User.all)
     end
   end
+
+  describe "DELETE#destroy" do
+
+    before(:example) do
+      @user = FactoryBot.create(:user)
+      @user.update_attribute("role", "admin")
+      @user1 = FactoryBot.create(:user)
+      @user2 = FactoryBot.create(:user)
+    end
+
+    it "should not render the template if the current user is not logged in" do
+      delete :destroy, params: {id: @user2.id}
+      expect(response.status).to eq(302)
+      expect(flash[:alert]).to eq("")
+      expect(response).to redirect_to(new_user_session_path)
+      expect(response).not_to render_template(:index)
+    end
+
+    it "should not render the template if the current user is not an admin" do
+      sign_in(@user1)
+      delete :destroy, params: {id: @user2.id}
+      expect(response.status).to eq(302)
+      expect(flash[:notice]).to eq("You do not have access to this page.")
+      expect(response).not_to render_template(:index)
+    end
+
+    it "should render the template if the current user is an admin" do
+      sign_in(@user)
+      delete :destroy, params: {id: @user2.id}
+      expect(response.status).to eq(200)
+      expect(response.content_type).to eq ("text/html")
+      expect(response).to render_template(:index)
+    end
+
+    it "should delete the target user from the database" do
+      prev_count = User.count
+      sign_in(@user)
+      delete :destroy, params: {id: @user2.id}
+      expect(User.count).to eq(prev_count - 1)
+      expect(User.all).to include(@user)
+      expect(User.all).to include(@user1)
+      expect(User.all).not_to include(@user2)
+    end
+
+    it "should provide an updated list of all the users" do
+      sign_in(@user)
+      patch :update, params: {id: @user2.id}
+      expect(assigns(:users)).to eq(User.all)
+    end
+  end
 end
